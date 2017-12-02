6a92f844c1a49fe323a5cf8269e05f7c9e895627
Marc Wilson <mwilson@marcswilson.com>
2017-12-01 22:40:31 -0800

    switched to legacy auth; implemented some endpoints

--- DocuSign/Controllers/DocuSignController.cs
+++ DocuSign/Controllers/DocuSignController.cs
@@ -1,5 +1,5 @@
 ﻿using DocuSign.eSign.Api;
-using DocuSign.eSign.Client;
+using DSC = DocuSign.eSign.Client;
 using DocuSign.eSign.Model;
 using System;
 using System.Collections.Generic;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Results;
+using System.IO;
 //using System.Web.Mvc;
 
 namespace DocuSign.Controllers
@@ -15,68 +16,113 @@ namespace DocuSign.Controllers
     {
         // GET: DocuSign
 		[HttpGet()]
-		[Route("Test")]
-        public JsonResult<EnvelopeSummary> Test()
+		[Route("send")]
+        public JsonResult<ViewUrl> Test()
         {
-			string userId = "ba2a2890-7b77-476c-9d75-52266bb879fa"; // use your userId (guid), not email address
-			string oauthBasePath = "account-d.docusign.com";
+			string username = "docusignmarc@gmail.com"; // use your userId (guid), not email address
+			string password = "docusignmarc123";
 			string integratorKey = "cd7de09e-6e24-4970-b909-624e875b8f68";
-			string privateKeyFilename = @"C:\Users\Marc\Source\Repos\DocuSign\DocuSign\PrivateKey.txt";
-			int expiresInHours = 1;
-			string host = "https://demo.docusign.net/restapi";
+			// initialize client for desired environment (for production change to www)
+			DSC.ApiClient apiClient = new DSC.ApiClient("https://demo.docusign.net/restapi");
+			DSC.Configuration.Default.ApiClient = apiClient;
 
-			string accountId = string.Empty;
+			// configure 'X-DocuSign-Authentication' header
+			string authHeader = "{\"Username\":\"" + username + "\", \"Password\":\"" + password + "\", \"IntegratorKey\":\"" + integratorKey + "\"}";
+			DSC.Configuration.Default.AddDefaultHeader("X-DocuSign-Authentication", authHeader);
 
-			ApiClient apiClient = new ApiClient(host);
-			apiClient.ConfigureJwtAuthorizationFlow(integratorKey, userId, oauthBasePath, privateKeyFilename, expiresInHours);
+			// we will retrieve this from the login API call
+			string accountId = null;
 
 			/////////////////////////////////////////////////////////////////
 			// STEP 1: LOGIN API        
 			/////////////////////////////////////////////////////////////////
-			AuthenticationApi authApi = new AuthenticationApi(apiClient.Configuration);
+
+			// login call is available in the authentication api 
+			AuthenticationApi authApi = new AuthenticationApi();
 			LoginInformation loginInfo = authApi.Login();
 
-			// find the default account for this user
-			foreach (LoginAccount loginAcct in loginInfo.LoginAccounts)
-			{
-				if (loginAcct.IsDefault == "true")
-				{
-					accountId = loginAcct.AccountId;
+			// parse the first account ID that is returned (user might belong to multiple accounts)
+			accountId = loginInfo.LoginAccounts[0].AccountId;
 
-					string[] separatingStrings = { "/v2" };
-
-					// Update ApiClient with the new base url from login call
-					apiClient = new ApiClient(loginAcct.BaseUrl.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries)[0]);
-					break;
-				}
-			}
+			// Update ApiClient with the new base url from login call
+			string[] separatingStrings = { "/v2" };
+			apiClient = new DSC.ApiClient(loginInfo.LoginAccounts[0].BaseUrl.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries)[0]);
 
 			/////////////////////////////////////////////////////////////////
 			// STEP 2: CREATE ENVELOPE API        
-			/////////////////////////////////////////////////////////////////				
+			/////////////////////////////////////////////////////////////////
 
+			// create a new envelope which we will use to send the signature request
 			EnvelopeDefinition envDef = new EnvelopeDefinition();
-			envDef.EmailSubject = "[DocuSign C# SDK] - Please sign this doc";
+			envDef.EmailSubject = "[DocuSign C# SDK] - Sample Signature Request";
+
+			// provide a valid template ID from a template in your account
+			// envDef.TemplateId = "7decf5f8-b499-4b51-8c3c-7c3a2702eefa";
+			Document doc = new Document();
+			Signer signer = new Signer();
+			Byte[] bytes = File.ReadAllBytes(@"C:\Users\Marc\Downloads\Mutual_NDA.pdf");
+			String file = Convert.ToBase64String(bytes);
+			doc.DocumentBase64 = file;
+			doc.DocumentId = "1";
+			doc.Name = "Mutual_NDA.pdf";
+			envDef.Documents = new List<Document>();
+			envDef.Documents.Add(doc);
+			signer.Email = username;
+			signer.Name = "Marc";
+			signer.RecipientId = "1";
+			signer.ClientUserId = "123";
+			envDef.Recipients = new Recipients();
+			envDef.Recipients.Signers = new List<Signer>();
+			envDef.Recipients.Signers.Add(signer);
+
 
 			// assign recipient to template role by setting name, email, and role name.  Note that the
 			// template role name must match the placeholder role name saved in your account template.  
 			TemplateRole tRole = new TemplateRole();
-			tRole.Email = "docusignmarc@gmail.com";
-			tRole.Name = "Mar";
-			tRole.RoleName = "SCP";
-			List<TemplateRole> rolesList = new List<TemplateRole>() { tRole };
+			tRole.Email = "mswilson4040@hotmail.com";
+			tRole.Name = "Marc";
+			tRole.RoleName = "Signer";
 
-			// add the role to the envelope and assign valid templateId from your account
+			// add the roles list with the our single role to the envelope
+			List<TemplateRole> rolesList = new List<TemplateRole>() { tRole };
 			envDef.TemplateRoles = rolesList;
-			envDef.TemplateId = "";
 
 			// set envelope status to "sent" to immediately send the signature request
 			envDef.Status = "sent";
 
 			// |EnvelopesApi| contains methods related to creating and sending Envelopes (aka signature requests)
-			EnvelopesApi envelopesApi = new EnvelopesApi(apiClient.Configuration);
+			EnvelopesApi envelopesApi = new EnvelopesApi();
 			EnvelopeSummary envelopeSummary = envelopesApi.CreateEnvelope(accountId, envDef);
-			return Json(envelopeSummary);
+			ViewUrl url = GetViewUrl(envelopesApi, envDef, envelopeSummary);
+			return Json(url);
+		}
+
+		[HttpGet()]
+		[Route("callback")]
+		public void ActionHandler()
+		{
+			var uri = this.Request.RequestUri;
+
+			var t = 1 + 1;
+			if (t == 2)
+			{
+
+			}
+		}
+
+		public ViewUrl GetViewUrl(EnvelopesApi envelopesApi, EnvelopeDefinition envelopeDefinition, EnvelopeSummary envelopeSummary)
+		{
+			RecipientViewRequest viewOptions = new RecipientViewRequest()
+			{
+				ReturnUrl = "http://localhost:49899/callback",
+				ClientUserId = "123",
+				AuthenticationMethod = "email",
+				UserName = envelopeDefinition.Recipients.Signers[0].Name,
+				Email = envelopeDefinition.Recipients.Signers[0].Email
+			};
+
+			ViewUrl url = envelopesApi.CreateRecipientView("4003313", envelopeSummary.EnvelopeId, viewOptions);
+			return url;
 		}
     }
 }
